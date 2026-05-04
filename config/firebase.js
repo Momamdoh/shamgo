3d96f49d33be665c878c050ae5a780846904d5e7
Mohamed <your@email.com>
2026-05-04 23:29:18 +0300

    remove env secrets

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../sham-go-e0a8b-firebase-adminsdk-fbsvc-9a0ef64ad9.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
