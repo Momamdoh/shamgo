440378111dd8123f781243fa9808197297df303b
Mohamed <your@email.com>
2026-05-29 02:47:52 +0300

    fix firebase service account env

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../sham-go-e0a8b-firebase-adminsdk-fbsvc-5c270d0e4a.json");
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
